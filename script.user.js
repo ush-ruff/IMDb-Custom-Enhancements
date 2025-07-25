77fe4cde8f8a93ae7e8d20219a270d2a984db849
ushruff <85804455+ush-ruff@users.noreply.github.com>
2025-07-25 10:49:17 +0500

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name        IMDB - Custom Enhancements
+// @namespace   Violentmonkey Scripts
+// @match       https://www.imdb.com/*
+// @grant       none
+// @version     0.1.0
+// @author      ushruff
+// @description Setup custom keyboard shortcuts
+// ==/UserScript==
+
+const KEYS = {
+  70:         () => focusSelectElement(`#suggestion-search`),                                                     // key: f
+  27:         () => clickElement(`.ipc-page-content-container[role="presentation"] a[href$="?ref_=mv_close"]`),   // key: esc
+  82:         () => clickElement(`[data-testid="hero-rating-bar__user-rating"] > .ipc-btn`),                      // key: r
+  'shift+82': () => clickElement(`.ipc-promptable-base__panel [data-testid="rate-button"]`),                      // key: shift + r
+}
+
+
+// -------------------------------------------
+// Event Listeners
+// -------------------------------------------
+document.addEventListener("keyup", pressKey)
+
+
+// -------------------------------------------
+// Main Functions
+// -------------------------------------------
+function pressKey(e) {
+  let key = e.keyCode
+
+  if (e.ctrlKey) key = `ctrl+${key}`
+  if (e.shiftKey) key = `shift+${key}`
+  if (e.altKey) key = `alt+${key}`
+
+  if (e.target.tagName == "INPUT" || e.target.tagName == "TEXTAREA") return
+
+  if (key in KEYS) {
+    return KEYS[key]()
+  }
+}
+
+
+// -------------------------------------------
+// Helper Functions
+// -------------------------------------------
+function focusSelectElement(element) {
+  const el = document.querySelector(element)
+
+  if (el !== null) {
+    el.focus()
+    el.select()
+  }
+}
+
+function clickElement(element) {
+  const el = document.querySelector(element)
+
+  if (el !== null) el.click()
+}
